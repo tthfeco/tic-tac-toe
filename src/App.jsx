eec3721bc7026eaea8159541285cbeea130a6d99
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-09-06 13:29:11 +0200

    create ol and 2 li for the players on app-jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 
 function App() {
   return <main>
-    <div id="game-conatiner">
-      Players
-
-      Game board
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player-name">Player</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">Player</span>
+          <span className="player-symbol">O</span>
+        </li>
+      </ol>
     </div>
     Log
   </main>
