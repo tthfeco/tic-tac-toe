de19bd1f8719e596b8ad942988266ea91c479857
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-09-07 09:11:53 +0200

    add parent span element for the second player

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-
 function App() {
-  return <main>
-    <div id="game-container">
-      <ol id="players">
-        <li>
-          <span className="player">
-          <span className="player-name">Player</span>
-          <span className="player-symbol">X</span>
-          </span>
-        </li>
-        <button>Edit</button>
-        <li>
-          <span className="player-name">Player</span>
-          <span className="player-symbol">O</span>
-        </li>
-        <button>Edit</button>
-      </ol>
-    </div>
-    Log
-  </main>
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player</span>
+              <span className="player-symbol">X</span>
+            </span>
+          </li>
+          <button>Edit</button>
+          <li>
+            <span className="player">
+              <span className="player-name">Player</span>
+              <span className="player-symbol">O</span>
+            </span>
+          </li>
+          <button>Edit</button>
+        </ol>
+      </div>
+      Log
+    </main>
+  );
 }
 
-export default App
+export default App;
