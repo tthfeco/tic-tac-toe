27d9fcc9078580c328f16e0b6f009599f9ccc0c3
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-09-07 09:10:33 +0200

    add edit button and parent span element for players

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ function App() {
     <div id="game-container">
       <ol id="players">
         <li>
+          <span className="player">
           <span className="player-name">Player</span>
           <span className="player-symbol">X</span>
+          </span>
         </li>
+        <button>Edit</button>
         <li>
           <span className="player-name">Player</span>
           <span className="player-symbol">O</span>
         </li>
+        <button>Edit</button>
       </ol>
     </div>
     Log
