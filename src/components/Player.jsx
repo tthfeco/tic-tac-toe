568e7ee1cca91a5ec2944f0aa87a632e5c5743be
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-09-09 10:52:02 +0200

    add handleclik and useState for the player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,19 @@
-export default function Player({ player, symbol}) {
+import { useState } from "react";
+
+export default function Player({ player, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleClik() {
+    setIsEditing(true);
+  }
+
   return (
     <li>
       <span className="player">
         <span className="player-name">{player}</span>
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={() => handleClik()}>Edit</button>
     </li>
   );
 }
